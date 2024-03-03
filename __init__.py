53e28f25619f9e163c165acc648b7f3143111af5
Artem Oleinikov <artem.oleinikov.web@gmail.com>
2024-03-03 10:18:47 +0200

    Add AddressBook, remove unnecessary

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from birthdays import get_birthdays_per_week
 from bot import main as run_bot
+from assistent import main as run_assistent
 
-__all__ = [get_birthdays_per_week, run_bot]
+__all__ = [run_bot, run_assistent]
